cc3b47798081a9b0da82eabe1847030408e404eb
Alex <strangedream1900@gmail.com>
2016-06-20 18:31:52 +0000

    adding settings

--- jakefile.js
+++ jakefile.js
@@ -1,8 +1,31 @@
-
+/* global desc, task, jake, fail, complete */
+"use strict";
 task("default", ["lint"]);
 
 desc("Lint everything");
 task("lint", [], function () {
     var lint = require('./build/lint/lint_runner.js');
-    lint.validateFile("jakefile.js", {}, {});
+
+    var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
+    files.exclude("build");
+	var options = {
+		bitwise: true,
+		curly: false,
+		eqeqeq: true,
+		forin: true,
+		immed: true,
+		latedef: true,
+		newcap: true,
+		noarg: true,
+		noempty: true,
+		nonew: true,
+		regexp: true,
+		undef: true,
+		strict: true,
+		trailing: true,
+		node: true
+	};
+    lint.validateFileList(files.toArray(), options, {});
 });
