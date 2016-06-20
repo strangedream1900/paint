854492d8c93c7c1cedb708d3ad34abb2464a582d
Alex <strangedream1900@gmail.com>
2016-06-20 18:41:49 +0000

    more settings

--- jakefile.js
+++ jakefile.js
@@ -1,31 +1,36 @@
 /* global desc, task, jake, fail, complete */
-"use strict";
-task("default", ["lint"]);
+(function () {
+    "use strict";
+    task("default", ["lint"]);
 
-desc("Lint everything");
-task("lint", [], function () {
-    var lint = require('./build/lint/lint_runner.js');
+    desc("Lint everything");
+    task("lint", [], function () {
+        var lint = require('./build/lint/lint_runner.js');
 
-    var files = new jake.FileList();
-    files.include("**/*.js");
-    files.exclude("node_modules");
-    files.exclude("build");
-	var options = {
-		bitwise: true,
-		curly: false,
-		eqeqeq: true,
-		forin: true,
-		immed: true,
-		latedef: true,
-		newcap: true,
-		noarg: true,
-		noempty: true,
-		nonew: true,
-		regexp: true,
-		undef: true,
-		strict: true,
-		trailing: true,
-		node: true
-	};
-    lint.validateFileList(files.toArray(), options, {});
-});
+        var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
+        files.exclude("build");
+        lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+    });
+
+    function nodeLintOptions() {
+        return {
+            bitwise: true,
+            curly: false,
+            eqeqeq: true,
+            forin: true,
+            immed: true,
+            latedef: false,
+            newcap: true,
+            noarg: true,
+            noempty: true,
+            nonew: true,
+            regexp: true,
+            undef: true,
+            strict: true,
+            trailing: true,
+            node: true
+        };
+    }
+}());
