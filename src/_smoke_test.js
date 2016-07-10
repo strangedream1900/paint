82be8a910e5e267dafc537e957cd6cbbbbdbf018
Alex <strangedream1900@gmail.com>
2016-07-10 20:48:50 +0000

    updated

--- src/_smoke_test.js
+++ src/_smoke_test.js
@@ -1,10 +1,13 @@
 "use strict";
 
+var jake = require("jake");
 var child_process = require("child_process");
 var http = require("http");
+var fs = require("fs");
+var procFile = require("procfile");
 var child;
 
-exports.setUp = function(done){
+exports.setUp = function (done) {
     runServer(done);
 };
 
@@ -18,32 +21,42 @@ exports.tearDown = function (done) {
 };
 
 exports.test_canGetHomePage = function (test) {
-        httpGet("http://192.168.129.140:8089", function (response, receivedData) {
-            var foundHomePage = receivedData.indexOf("WeeWikiPaint home page") !== -1;
-            test.ok(foundHomePage, "home page should have contained test marker");
-            test.done();
+    httpGet("http://192.168.129.140:5000", function (response, receivedData) {
+        var foundHomePage = receivedData.indexOf("WeeWikiPaint home page") !== -1;
+        test.ok(foundHomePage, "home page should have contained test marker");
+        test.done();
     });
 };
 
-exports.test_canGet404Page = function(test) {
-            httpGet("http://192.168.129.140:8089/nonexistant.html", function (response, receivedData) {
-            var foundHomePage = receivedData.indexOf("WeeWikiPaint 404 page") !== -1;
-            test.ok(foundHomePage, "404 page should have contained test marker");
-            test.done();
+exports.test_canGet404Page = function (test) {
+    httpGet("http://192.168.129.140:5000/nonexistant.html", function (response, receivedData) {
+        var foundHomePage = receivedData.indexOf("WeeWikiPaint 404 page") !== -1;
+        test.ok(foundHomePage, "404 page should have contained test marker");
+        test.done();
     });
 };
 
 // TODO: check the 404 page
 
 function runServer(callback) {
-    child = child_process.spawn("node", ["src/server/wikipaint", "8089"]);
+    var commandLine = parseProcFile();
+    child = child_process.spawn(commandLine.command, commandLine.options);
     child.stdout.setEncoding("utf8");
     child.stdout.on("data", function (chunk) {
-        process.stdout.write("server stdout: " + chunk);
-        if (chunk.trim() === "Server started") {
+        if (chunk.trim().indexOf("Server started") !== -1) {
             callback();
         }
     });
+}
+
+function parseProcFile() {
+    var fileData = fs.readFileSync("Procfile", "utf8");
+    var webCommand = procFile.parse(fileData).web;
+    webCommand.options = webCommand.options.map(function (element) {
+        if (element === "$PORT") { return "5000"; }
+        else { return element; }
+    });
+    return webCommand;
 }
 
 function httpGet(url, callback) {
